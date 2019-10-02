1df4f9972bc57a90efd920748c0f8ab7422e1e8f
Samyak Jain <44099124+Samyak2607@users.noreply.github.com>
2019-10-02 13:13:03 +0530

    Head and Tail

--- Toss.py
+++ Toss.py
@@ -1,9 +1,30 @@
 import numpy as np
-print('Choose Head or Tail')
-choice = input()
-np.random.seed(123)
-coin = np.random.randint(0,2)
-if(coin == 0 ):
-  print('Head comes')
+
+name_1st_person = input('Team A : Enter Your Team Name')
+name_2nd_person = input('Team B: Enter Opponent Team Name')
+
+ch = np.random.randint(0,2)
+choice = ''
+if(ch == 0):
+	choice = input(name_1st_person+' Select Head or Tail')
 else:
-  print('Tails Comes')
+	choice = input(name_2nd_person+' select Head or Tail')
+
+np.random.seed(12)
+
+coin = np.random.randint(0,2)
+
+if(choice.lower() != 'head' or choice.lower() != 'tail'):
+	print('Invalid Input')
+
+elif((coin == 0 and choice.lower() == 'head') or (coin == 1 and choice.lower() == 'tail')):
+	if(ch == 0):
+		print(name_1st_person+' Team Win')
+	else:
+		print(name_2nd_person+' Team Win')
+else:
+	if(ch == 0):
+		print(name_2nd_person+' Team Win')
+	else:
+		print(name_1st_person+' Team Win')
+
